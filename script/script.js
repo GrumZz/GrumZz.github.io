05042ae4f86d2cddbf37616a0dd3b8c12ed2a5c9
GrumZz <95189155+GrumZz@users.noreply.github.com>
2022-10-12 19:50:49 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,28 +1,24 @@
-// и нахуй ты сюда зашел?
-
 function generatePassword (passwordLength) {
-    let numberChars = "0123456789";                         //
-    let upperChars = "ABCDEFGHIJKLMNOPQRSTVWXYZ";           //
-    let lowerChars = "abcdefghijklomnoprstvwxyz";           //
-    let symbolsChars = "!~$%&/()=?'*+<>|#_-:.;{[]}@^";       // это крч все буковки и цифорки для ген пароля 
-    let allChars = numberChars + upperChars + lowerChars + symbolsChars; // это чтобы их всех склеить вместе
+    let numberChars = "0123456789"; 
+    let upperChars = "ABCDEFGHIJKLMNOPQRSTVWXYZ";
+    let lowerChars = "abcdefghijklomnoprstvwxyz";      
+    let symbolsChars = "!~$%&/()=?'*+<>|#_-:.;{[]}@^";
+    let allChars = numberChars + upperChars + lowerChars + symbolsChars;
     let randPasswordArray = Array(passwordLength); 
     randPasswordArray[0] = numberChars;
     randPasswordArray[1] = upperChars;
     randPasswordArray[2] = lowerChars;
-    randPasswordArray[3] = symbolsChars;                // не ебу что это
+    randPasswordArray[3] = symbolsChars;
     randPasswordArray= randPasswordArray.fill(allChars, 3);
-    return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join(''); // это код
+    return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
 }
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() + (i + 1));
-        let temp = array[i];                                // смешиватель буковок и цифорок
+        let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
     return array;
 }
-
-// поставь лайк за 5 сек или сдохнешь
